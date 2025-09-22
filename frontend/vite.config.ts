05cd4aed860bc01ef0cd0ae84d6707527779ba3c
Purgato96 <matheus_purgato@hotmail.com>
2025-09-22 10:15:37 -0300

    Parametrizando componentes de frontend e de backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -9,10 +11,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
